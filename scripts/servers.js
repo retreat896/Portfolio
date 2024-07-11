17d0e03a0891c3c10933962306d5bfd089c98a0d
Kris <kris.adams3000@gmail.com>
2024-07-11 11:56:28 -0500

    added a handler for offline servers

--- scripts/servers.js
+++ scripts/servers.js
@@ -13,6 +13,40 @@ const ajaxSettings = {
     $.ajax(ajaxSettings).done(function (response) {
         for (let server in response) {
           if (response[server].error) {
+            let example = `
+              <div class="card mb-3">
+                <div class="card-body flexRight">
+                  <div class="flexDown center">
+                    ${serverName}
+                    <img class="gameCover" alt="${serverName}" title="${serverName}" src="${serverCover}">
+                  </div>
+                  <div class="table-responsive">
+                    <table class="table table-hover flexRight" summary="${serverName} Server">
+                      <thead>
+                        <tr class="flexDown">
+                          <th class="borderUnderline" scope="col">Server Name:</th>
+                          <th class="borderUnderline" scope="col">IP Address:</th>
+                          <th class="borderUnderline" scope="col">Players:</th>
+                          <th class="borderUnderline" scope="col">Message:</th>
+                          <th class="borderUnderline" scope="col">Error:</th>
+                        </tr>
+                      </thead>
+                      <tbody class="flexRight">
+                        <tr class="flexDown">
+                          <td>${serverName||"null"}</td>
+                          <td>null</td>
+                          <td>0/0</td>
+                          <td>${response[server].message}</td>
+                          <td>${response[server].error}</td>
+
+                        </tr>
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+              </div>`
+              $("#serverCards").append(example);
+          
             console.log(response[server].error);
             console.log(response[server].message);
           } else {
