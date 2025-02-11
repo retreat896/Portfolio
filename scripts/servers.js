aa8a5bd5b1e70f701a5f4c2b130d9e334b847cf4
kris <adamskri@uwplatt.edu>
2025-02-11 15:26:24 -0600

    adjusted stuff

--- scripts/servers.js
+++ scripts/servers.js
@@ -1,25 +1,27 @@
 const ajaxSettings = {
-  "async": true,
-  "crossDomain": true,
-  "url": "https://api.retreat896.com/servers",
-  "method": "GET",
-  "headers": {
-    "Content-Type": "application/json",
-    "Accept": "application/json",
-    "Access-Control-Allow-Origin": "*",
-  }
+    async: true,
+    crossDomain: true,
+    url: 'https://api.retreat896.com/servers',
+    method: 'GET',
+    headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+        'Access-Control-Allow-Origin': '*',
+    },
 };
 function getServerStatus() {
-  $.ajax(ajaxSettings)
-  .done(function (response) {
-    for (let server in response) {
-      if (response[server].error) {
-        let serverData = response[server];
-        let serverName = server; // Use the server key as the name
-        let serverCover = './images/' + serverName.toLowerCase() + '.jpg';
-        if(serverName == 'sevenDays'){serverName = '7 Days to Die'}
-        console.log(serverName);
-        let example = `
+    $.ajax(ajaxSettings)
+        .done(function (response) {
+            for (let server in response) {
+                if (response[server].error) {
+                    let serverData = response[server];
+                    let serverName = server; // Use the server key as the name
+                    let serverCover = './images/' + serverName.toLowerCase() + '.jpg';
+                    if (serverName == 'sevenDays') {
+                        serverName = '7 Days to Die';
+                    }
+                    console.log(serverName);
+                    let example = `
   <div class="card mb-3">
     <div class="card-body flexRight">
       <div class="flexDown center gameCoverContainer">
@@ -27,7 +29,7 @@ function getServerStatus() {
         <img class="gameCover" alt="${serverName}" title="${serverName}" src="${serverCover}">
       </div>
       <div class=" serverInfoContainer">
-        <table class="table table-hover " summary="${serverName} Server">
+        <table class="table table-striped table-hover " summary="${serverName} Server">
           <tbody>
             <tr>
               <th class=" border-0 bg-danger rounded-start" scope="row">Status:</th>
@@ -35,7 +37,7 @@ function getServerStatus() {
             </tr>
             <tr>
               <th class="rounded-start border-0" scope="row">Server Name:</th>
-              <td class="rounded-end border-0">${serverName.slice(0, 1).toUpperCase() + serverName.slice(1) || "null"}</td>
+              <td class="rounded-end border-0">${serverName.slice(0, 1).toUpperCase() + serverName.slice(1) || 'null'}</td>
             </tr>
             <tr>
               <th class="rounded-start border-0" scope="row">IP Address:</th>
@@ -57,17 +59,19 @@ function getServerStatus() {
         </table>
       </div>
     </div>
-  </div>`
-        $("#serverCards").append(example);
+  </div>`;
+                    $('#serverCards').append(example);
 
-        console.log(response[server].error);
-        console.log(response[server].message);
-      } else {
-        let serverData = response[server];
-        let serverName = server; // Use the server key as the name
-        let serverCover = './images/' + serverName.toLowerCase() + '.jpg'; // Assuming cover images are named after the server keys
-        if(serverName == 'sevenDays'){serverName = '7 Days to Die'}
-        let example = `
+                    console.log(response[server].error);
+                    console.log(response[server].message);
+                } else {
+                    let serverData = response[server];
+                    let serverName = server; // Use the server key as the name
+                    let serverCover = './images/' + serverName.toLowerCase() + '.jpg'; // Assuming cover images are named after the server keys
+                    if (serverName == 'sevenDays') {
+                        serverName = '7 Days to Die';
+                    }
+                    let example = `
   <div class="card mb-3">
     <div class="card-body flexRight">
       <div class="flexDown center gameCoverContainer">
@@ -75,7 +79,7 @@ function getServerStatus() {
         <img class="gameCover" alt="${serverName}" title="${serverName}" src="${serverCover}">
       </div>
       <div class=" serverInfoContainer">
-        <table class="table table-hover " summary="${serverName} Server">
+        <table class="table table-striped table-hover" summary="${serverName} Server">
           <tbody>
             <tr>
               <th class="bg-success rounded-start" scope="row">Status:</th>
@@ -83,50 +87,49 @@ function getServerStatus() {
             </tr>
             <tr>
               <th class="rounded-start border-0" scope="row">Server Name:</th>
-              <td class="rounded-end border-0">${(serverData.name).slice(0, 1).toUpperCase() + serverData.name.slice(1) || serverName.slice(0, 1).toUpperCase() + serverName.slice(1) || "null"}</td>
+              <td class="rounded-end border-0">${serverData.name.slice(0, 1).toUpperCase() + serverData.name.slice(1) || serverName.slice(0, 1).toUpperCase() + serverName.slice(1) || 'null'}</td>
             </tr>
             <tr>
               <th class="rounded-start border-0" scope="row">IP Address:</th>
-              <td class="rounded-end border-0">${serverData.connect || "null"}</td>
+              <td class="rounded-end border-0">${serverData.connect || 'null'}</td>
             </tr>
             <tr>
               <th class="rounded-start border-0" scope="row">Players:</th>
-              <td class="rounded-end border-0">${serverData.numplayers}/${serverData.maxplayers || "null"}</td>
+              <td class="rounded-end border-0">${serverData.numplayers}/${serverData.maxplayers || 'null'}</td>
             </tr>
             <tr>
               <th class="rounded-start border-0" scope="row">Ping:</th>
-              <td class="rounded-end border-0">${serverData.ping || "null"}</td>
+              <td class="rounded-end border-0">${serverData.ping || 'null'}</td>
             </tr>
             <tr>
               <th class="rounded-start border-0" scope="row">Version:</th>
-              <td class="rounded-end border-0">${serverData.version || serverData.raw.version.name || "null"}</td>
+              <td class="rounded-end border-0">${serverData.version || serverData.raw.version.name || 'null'}</td>
             </tr>
           </tbody>
         </table>
       </div>
     </div>
   </div>`;
-        console.log(serverData.numplayers);
-        $("#serverCards").prepend(example);
-      }
-    }
-    $('.placeholder-wave').removeClass('placeholder-wave');
-    $('.placeholder-glow').removeClass('placeholder-glow');
-    $('.placeholder').removeClass('placeholder');
-    $("#loadingCard").remove();
-  })
-  .fail(function(){
-    $("#serverCards").find(".placeholder:not('.gameCoverContainer'), .placehodler-sm:not('.gameCoverContainer')").each(function(){
-      console.log($(this).text())
-      $(this).text("Error").addClass("bg-danger rounded-end").removeClass("placeholder placeholder-sm")
-
-    })
-    $("#serverCards")
-    .find(".gameCoverContainer").removeClass("placeholder placeholder-sm")
-    .html("<img class='rounded mx-auto d-block' alt='...' title='...' src='./images/filler.png' width='100%'>")
-  })
+                    console.log(serverData.numplayers);
+                    $('#serverCards').prepend(example);
+                }
+            }
+            $('.placeholder-wave').removeClass('placeholder-wave');
+            $('.placeholder-glow').removeClass('placeholder-glow');
+            $('.placeholder').removeClass('placeholder');
+            $('#loadingCard').remove();
+        })
+        .fail(function () {
+            $('#serverCards')
+                .find(".placeholder:not('.gameCoverContainer'), .placehodler-sm:not('.gameCoverContainer')")
+                .each(function () {
+                    console.log($(this).text());
+                    $(this).text('Error').addClass('bg-danger rounded-end').removeClass('placeholder placeholder-sm');
+                });
+            $('#serverCards').find('.gameCoverContainer').removeClass('placeholder placeholder-sm').html("<img class='rounded mx-auto d-block' alt='...' title='...' src='./images/filler.png' width='100%'>");
+        });
 }
 
 $(document).ready(function () {
-  getServerStatus()
-})
+    getServerStatus();
+});
